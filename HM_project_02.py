0379844b9513455c91cfe6ff8ead4755b245e10b
Hgmedina29 <Hgmedina519@gmail.com>
2025-01-28 13:36:51 -0500

    1/28/25 H&S classwork

---
+++ HM_project_02.py
@@ -0,0 +1,23 @@
+def conversation():
+	print("I will keep asking questions until you type 'exit'")
+	print("1. What is your name?")
+	name=input()
+	print("2. What is your favorite show, {}?".format(name))
+	show=input()
+	print("3. Do you like programming, {}?".format(name))
+	answer=input()
+
+def greeting():
+	print("Welcome to my conversation program")
+	conversation()
+	print("Thanks for chatting with me")
+	
+def main():
+	count=0
+	run_loop=conversation()
+
+	while run_loop!='exit':
+		run_loop=conversation()
+
+if __name__ == "__main__":
+		main()
--- Lab10.py
+++ Lab10.py
@@ -4,7 +4,7 @@ def main():
 	num1=input("Enter first number:")
 	num2=input("Enter second number:")
 
-	total=num1=num2
+	total = num1 + num2
 
 	print("{}+{}={}".format(num1,num2,total))
 
